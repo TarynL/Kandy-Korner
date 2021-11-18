c9645bd113a4a9326c42c06878e77d52796973d4
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-11-18 11:45:36 -0600

    locationList and productList with type are displaying

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { LocationList } from "./locations/LocationList"
+import { ProductList } from "./products/ProductList"
+
+
+export const KandyKorner = () => {
+
+
+    return (
+        <>
+            <h1>Kandy Korner</h1>
+            <h2>Location List</h2>
+            <LocationList />
+            <h2>Product List</h2>
+            <ProductList />
+
+
+        </>
+    )
+}
---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+export const LocationList = () => {
+    const [locations, setLocations] = useState([])
+
+    useEffect(
+        () => {
+            fetch('http://localhost:8088/locations')
+                .then(res => res.json())
+                .then(
+                    (locations) => {
+                        setLocations(locations)
+                    }
+                )
+        },
+        []
+    )
+
+
+    return (
+        <>
+            {
+                locations.map(
+                    (location) => {
+                        return <p key={location.id}>{location.address}</p>
+                    }
+                )
+            }
+        </>
+    )
+}
