9df0d5b7ac5e0863e128a7d16c446aefd42d1265
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-11-22 11:28:48 -0600

    added navbar/login/register

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,20 +1,33 @@
-import React from "react"
-import { LocationList } from "./locations/LocationList"
-import { ProductList } from "./products/ProductList"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./KandyKorner.css";
 
+export const KandyKorner = () => (
+    <>
+        <Route
+            render={() => {
+                if (localStorage.getItem("kandy_customer")) {
+                    return (
+                        <>
+                            <NavBar />
+                            <ApplicationViews />
+                        </>
+                    );
+                } else {
+                    return <Redirect to="/login" />;
+                }
+            }}
+        />
 
-export const KandyKorner = () => {
-
-
-    return (
-        <>
-            <h1>Kandy Korner</h1>
-            <h2>Location List</h2>
-            <LocationList />
-            <h2>Product List</h2>
-            <ProductList />
-
-
-        </>
-    )
-}
+        <Route path="/login">
+            <Login />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
+    </>
+);
